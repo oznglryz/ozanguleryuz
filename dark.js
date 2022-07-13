c8755883d58bab67495ac904d48686b2c1dbd1a8
Ozan <guleryuzozan@gmail.com>
2022-07-13 06:30:15 +0300

    update

--- dark.js
+++ dark.js
@@ -2,26 +2,35 @@ const themeToggle = document.querySelector(
   '.theme-switch input[type="checkbox"]'
 );
 function switchTheme(e) {
-  if (e.target.checked) {
+  if (e.target.checked) 
+  {
     document.documentElement.setAttribute("data-theme", "dark");
-  } else {
+  } 
+  else 
+  {
     document.documentElement.setAttribute("data-theme", "light");
   }
 }
 themeToggle.addEventListener("change", switchTheme, false);
-function switchTheme(e) {
-  if (e.target.checked) {
+function switchTheme(e) 
+{
+  if (e.target.checked) 
+  {
     document.documentElement.setAttribute("data-theme", "dark");
     localStorage.setItem("theme", "dark");
-  } else {
+  } 
+  else 
+  {
     document.documentElement.setAttribute("data-theme", "light");
     localStorage.setItem("theme", "light");
   }
 }
 const currentTheme = localStorage.getItem("theme");
-if (currentTheme) {
+if (currentTheme) 
+{
   document.documentElement.setAttribute("data-theme", currentTheme);
-  if (currentTheme === "dark") {
+  if (currentTheme === "dark") 
+  {
     themeToggle.checked = true;
   }
 }
